d8a89f3bf5e1e14a94100e38842472f66691af80
vinay.bhide <vinaybhide@hotmail.com>
2023-04-18 13:49:51 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpochToNormal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDateTime;
+
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(long.Parse(args[0]));
+            string timeZoneId = findTimeZoneId(args[1]);
+            TimeZoneInfo currentTimeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            localDateTime = TimeZoneInfo.ConvertTimeFromUtc(dateTimeOffset.UtcDateTime, currentTimeZone);
+            Console.WriteLine(localDateTime.ToString());
+        }
+
+        public static string findTimeZoneId(string zoneId)
+        {
+            string returnTimeZoneId = "";
+            switch (zoneId)
+            {
+                case "IST":
+                    returnTimeZoneId = "India Standard Time";
+                    break;
+                default:
+                    returnTimeZoneId = "India Standard Time";
+                    break;
+            }
+            return returnTimeZoneId;
+        }
+
+    }
+}
